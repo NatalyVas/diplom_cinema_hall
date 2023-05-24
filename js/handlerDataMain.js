80b4953ced59d901bc3310fd10ac2c5ced1b1a68
Nataly Vasilyeva <nvasilyeva.nv@gmail.com>
2023-05-24 12:12:18 +0500

    com

--- js/handlerDataMain.js
+++ js/handlerDataMain.js
@@ -27,7 +27,7 @@ function handlerDataMain(request) {
 			SendRequest(`POST`, `https://jscp-diplom.netoserver.ru/`, `event=get_hallConfig&timestamp=${timestamp}&hallId=${timeSeances[0].seance_hallid}&seanceId=${timeSeances[0].seance_id}`, handlerDataHall);
 
 	 		function handlerDataHall(request) {
-	 			let hallScheme = request.response.innerHTML;
+	 			let hallScheme = JSON.parse(request.response);
 	 			console.log(hallScheme);
 	 			let dataAll = [];
 	 			dataAll.push(data);
