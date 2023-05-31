d5da8c6f15de3c6c6bfbe1683e64cafac73a08e6
Nataly Vasilyeva <nvasilyeva.nv@gmail.com>
2023-05-31 16:51:36 +0500

    разобралась с выходными

--- js/index.js
+++ js/index.js
@@ -8,6 +8,11 @@ const chosen = document.querySelector(`.page-nav`).querySelector(`.page-nav__day
 chosen.classList.remove(`page-nav__day_chosen`);
 nav[0].classList.add(`page-nav__day_chosen`);
 
+const weekend = Array.from(document.querySelector(`.page-nav`).querySelectorAll(`.page-nav__day_weekend`));
+for (let w of weekend) {
+	w.classList.remove(`page-nav__day_weekend`);
+}
+
 const now = new Date();
 localStorage.setItem(`cinema`, JSON.stringify(now));
 
@@ -33,15 +38,14 @@ for (let i = 0; i < nav.length; i++) {
 
 	nav[i].querySelector(`.page-nav__day-week`).textContent = dateForNav.week;
 	nav[i].querySelector(`.page-nav__day-number`).textContent = dateForNav.day;
+	if (dateForNav.week === `сб` || dateForNav.week === `вс`) {
+		nav[i].classList.add(`page-nav__day_weekend`);
+	}
 
 
 	nav[i].addEventListener(`click`, () => {
 		document.querySelector(`.page-nav`).querySelector(`.page-nav__day_chosen`).classList.remove(`page-nav__day_chosen`);
 		nav[i].classList.add(`page-nav__day_chosen`);
-
-		main[0].innerHTML = ``;
-
-		SendRequest(`POST`, `https://jscp-diplom.netoserver.ru/`, `event=update`, handlerDataMain);
 
 		localStorage.setItem(`cinema`, JSON.stringify(date));
 
